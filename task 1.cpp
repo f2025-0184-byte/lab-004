393813a794b971958e14148630872f17645118b6
f2025-0184-byte <f2025-0184@bnu.edu.pk>
2025-10-24 15:04:49 +0500

    Create task 1.cpp

---
+++ task 1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.int main() {
+    cout << "Name:\t Zainab Alizai\n";
+    cout << "Department:\t Scit\n";
+    cout << "Quote\t\"knowledge is power\"\n";
+
+    return 0;
